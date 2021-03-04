3c991516fd98a8539dc53478b7a223409a3c66b3
gromov-nikita <hromay.n999@gmail.com>
2021-03-04 23:34:17 +0300

    1

--- 3)/src/Runner.java
+++ 3)/src/Runner.java
@@ -1,5 +1,11 @@
 public class Runner {
     public static void main(String[] args) {
-
+        int i = 0;
+        while(i <= 50) {
+            if(i % 9 == 0) {
+                System.out.println(i + " ");
+            }
+            i++;
+        }
     }
 }
